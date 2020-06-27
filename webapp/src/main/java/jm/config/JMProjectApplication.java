e7d9592819d1cef7bb41a287b0f387958706687e
Rustam <rustamg070619@gmail.com>
2020-06-27 11:52:11 +0300

    structure creation

---
+++ webapp/src/main/java/jm/config/JMProjectApplication.java
@@ -0,0 +1,14 @@
+package jm.config;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JMProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JMProjectApplication.class, args);
+    }
+
+}
