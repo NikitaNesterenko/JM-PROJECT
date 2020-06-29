15361de72048a30cc34b5904c3002af332d4cdd2
VasKV <vasily.262@gmail.com>
2020-06-30 01:43:35 +0600

    to run project

--- webapp/src/main/java/jm/config/JMProjectApplication.java
+++ webapp/src/main/java/jm/config/JMProjectApplication.java
@@ -3,9 +3,10 @@ package jm.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("jm")
 public class JMProjectApplication {
 
     public static void main(String[] args) {
