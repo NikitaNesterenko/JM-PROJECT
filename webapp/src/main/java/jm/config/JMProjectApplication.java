a0713ca49565d915da3a1da9c67369260d76fafd
VasKV <vasily.262@gmail.com>
2020-06-29 23:28:28 +0600

    to run project

--- webapp/src/main/java/jm/config/JMProjectApplication.java
+++ webapp/src/main/java/jm/config/JMProjectApplication.java
@@ -4,6 +4,7 @@ package jm.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class JMProjectApplication {
 
