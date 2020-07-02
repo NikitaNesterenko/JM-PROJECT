3592a3ed9764ca4f757b8b6dcbb4311216f7b9c1
Konstantin <linuksoid@gmail.com>
2020-07-03 02:27:02 +0300

    getCurrentUser in UserRestController created

--- webapp/src/main/java/jm/controller/user/UserRestController.java
+++ webapp/src/main/java/jm/controller/user/UserRestController.java
@@ -1,9 +1,20 @@
 package jm.controller.user;
 
+import jm.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/rest/api/users")
 public class UserRestController {
+
+    @GetMapping(value = "/getUser")
+    public ResponseEntity<User> getCurrentUser() {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
 }
