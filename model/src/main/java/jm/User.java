114601e16274860d4f7bc91ebab497082655a721
belogolov <belogolov.timur@yandex.ru>
2020-07-03 15:08:05 +0300

    Изменил модель User (добавил реализацию UserDetails) Прописал зависимость jjwt в pom(security) Добавил свойства (secret & sessionTime) Настроил работу с jwt токеном Добавил UserRepository для получения даных о пользователе

--- model/src/main/java/jm/User.java
+++ model/src/main/java/jm/User.java
@@ -3,15 +3,20 @@ package jm;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
 
 @Getter
 @Setter
 @ToString
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @Column(name = "id")
@@ -57,4 +62,40 @@ public class User {
         this.sellerLevel = sellerLevel;
         this.vacationMode = vacationMode;
     }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return new ArrayList<>();
+    }
+
 }
