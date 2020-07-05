ab51b0dfd40d16dd02c336fe6083fa8f63b99734
Konstantin <linuksoid@gmail.com>
2020-07-06 02:33:43 +0300

    remake AdminRestController with Response and Swagger

--- webapp/src/main/java/jm/controller/admin/AdminRestController.java
+++ webapp/src/main/java/jm/controller/admin/AdminRestController.java
@@ -1,15 +1,21 @@
 package jm.controller.admin;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jm.User;
 import jm.UserService;
+import jm.component.Response;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/rest/api/admin")
+@Tag(name = "admin", description = "Admin API")
 public class AdminRestController {
 
     private final UserService userService;
@@ -18,36 +24,86 @@ public class AdminRestController {
         this.userService = userService;
     }
 
-    @GetMapping(value = "/getAllUsers")
-    public ResponseEntity<List<User>> getAllUsers() {
+    @GetMapping
+    @Operation(
+            operationId = "getAllUsers",
+            summary = "Get all users",
+            responses = {
+                    @ApiResponse(responseCode = "200",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(type = "array", implementation = User.class)
+                            ),
+                            description = "OK: user list received"
+                    ),
+                    @ApiResponse(responseCode = "400", description = "NOT_FOUND: no users found")
+            })
+    public Response<List<User>> getAllUsers() {
         List<User> users = userService.getAllUsers();
-        return new ResponseEntity<>(users, HttpStatus.OK);
+        if (users != null) {
+            return Response.ok(users);
+        }
+        return Response.error(HttpStatus.BAD_REQUEST, "No users found");
     }
 
+    @Operation(
+            operationId = "createUser",
+            summary = "Create user",
+            responses = {
+                    @ApiResponse(
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = User.class)
+                            )
+                    ),
+                    @ApiResponse(responseCode = "200", description = "OK: user created"),
+                    @ApiResponse(responseCode = "400", description = "NOT_FOUND: user not created")
+            })
     @PostMapping(value = "/createUser")
-    public ResponseEntity<?> createUser(User user) {
+    public Response<?> createUser(User user) {
         if (userService.getUserByUserName(user.getUsername()) != null) {
-            return new ResponseEntity<>("This user already exists in database", HttpStatus.CONFLICT);
+            return Response.error(HttpStatus.BAD_REQUEST, "Error when creating a user");
         }
         userService.createUser(user);
-        return new ResponseEntity<>("User created successfully", HttpStatus.CREATED);
+        return Response.ok(HttpStatus.OK, "User created successfully");
     }
 
     @PutMapping(value = "/updateUser")
-    public ResponseEntity<?> updateUser(User user) {
+    @Operation(
+            operationId = "updateUser",
+            summary = "Update user",
+            responses = {
+                    @ApiResponse(
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = User.class)
+                            )
+                    ),
+                    @ApiResponse(responseCode = "200", description = "OK: user updated successfully"),
+                    @ApiResponse(responseCode = "400", description = "NOT_FOUND: unable to update user")
+            })
+    public Response<?> updateUser(User user) {
         if (userService.getUserByUserName(user.getUsername()) == null) {
-            return new ResponseEntity<>("User not found", HttpStatus.NOT_FOUND);
+            return Response.error(HttpStatus.BAD_REQUEST, "User not found");
         }
         userService.updateUser(user);
-        return new ResponseEntity<>("User updated successfully", HttpStatus.OK);
+        return Response.ok().build();
     }
 
     @DeleteMapping(value = "/deleteUser")
-    public ResponseEntity<?> deleteUser(Long id) {
+    @Operation(
+            operationId = "deleteUser",
+            summary = "Delete user",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "OK: user deleted successfully"),
+                    @ApiResponse(responseCode = "400", description = "NOT FOUND: no user with such id")
+            }
+    )
+    public Response<Boolean> deleteUser(Long id) {
         if (userService.getUserById(id) == null) {
-            return new ResponseEntity<>("User not found", HttpStatus.NOT_FOUND);
+            return Response.error(HttpStatus.BAD_REQUEST, "User not found"); //нужно ли тут сообщение?
         }
         userService.deleteUser(id);
-        return new ResponseEntity<>("User deleted successfully", HttpStatus.OK);
+        return Response.ok(true); //почему тут так? почему нет статуса и строки?
     }
 }
