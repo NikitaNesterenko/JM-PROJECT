e4e91fcd5925461210d3488d48304ff161f78db0
Konstantin <linuksoid@gmail.com>
2020-07-06 02:46:00 +0300

    remake UserRestController with Response and Swagger

--- webapp/src/main/java/jm/controller/user/UserRestController.java
+++ webapp/src/main/java/jm/controller/user/UserRestController.java
@@ -1,8 +1,12 @@
 package jm.controller.user;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jm.User;
+import jm.component.Response;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,9 +16,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/rest/api/users")
 public class UserRestController {
 
-    @GetMapping(value = "/getUser")
-    public ResponseEntity<User> getCurrentUser() {
+    @GetMapping(value = "/getLoggedInUser")
+    @Operation(
+            operationId = "getLoggedInUser",
+            summary = "Get logged-in user",
+            responses = {
+                    @ApiResponse(responseCode = "200",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = User.class)
+                            ),
+                            description = "OK: get logged-in user"
+                    )
+            })
+    public Response<User> getLoggedInUser() {
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        return new ResponseEntity<>(user, HttpStatus.OK);
+        if (user == null) {
+            return Response.error(HttpStatus.BAD_REQUEST, "Error login");
+        }
+        return Response.ok(user);
     }
 }
