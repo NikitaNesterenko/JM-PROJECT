5cd380618b55d2ab8b2d3f0160dba95c3c32511d
yuriy.kadysev <yuriy.kadysev@gmail.com>
2020-07-06 21:35:27 +0300

    EntityDataInitializer refactor

--- webapp/src/main/java/jm/config/JMProjectApplication.java
+++ webapp/src/main/java/jm/config/JMProjectApplication.java
@@ -1,13 +1,25 @@
 package jm.config;
 
 
+import jm.config.initializer.EntityDataInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 @EntityScan("jm")
+@ComponentScan("jm")
 public class JMProjectApplication {
+
+    @Bean(initMethod = "init")
+    @PostConstruct
+    public EntityDataInitializer initEntityData() {
+        return new EntityDataInitializer();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(JMProjectApplication.class, args);
