706402dcfcd66d1f7dcf8e6d7e0057c9d49dab84
belogolov <belogolov.timur@yandex.ru>
2020-07-13 17:07:16 +0300

    Добавил логику восстановления пароля пользователя по почте. Добавил тесты для MailServiceImpl, TokenRecoveryServiceImpl и UserServiceImpl.getUserByEmail().

--- service-abstract/src/main/java/jm/MailService.java
+++ service-abstract/src/main/java/jm/MailService.java
@@ -3,4 +3,9 @@ package jm;
 public interface MailService {
 
     void sendSimpleMessage(String to, String subject, String text);
+
+    boolean sendRecoveryLinkToUser(User user);
+
+    boolean changePasswordByToken(String link, String newPassword);
+
 }
---
+++ service-impl/src/test/java/MailServiceTest.java
@@ -0,0 +1,80 @@
+import jm.TokenRecovery;
+import jm.TokenRecoveryService;
+import jm.User;
+import jm.UserService;
+import jm.mailservice.MailServiceImpl;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.Date;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+@RunWith(SpringRunner.class)
+public class MailServiceTest {
+
+    @InjectMocks
+    private MailServiceImpl mailService;
+
+    @Mock
+    private JavaMailSender mailSender;
+
+    @Mock
+    private TokenRecoveryService tokenService;
+
+    @Mock
+    private UserService userService;
+
+    @Test
+    public void sendRecoveryLinkToUserTest() {
+        User user = new User();
+        user.setEmail("testEmail");
+        assertTrue(mailService.sendRecoveryLinkToUser(user));
+    }
+
+    @Test
+    public void doNotSendRecoveryLinkToNullUserTest() {
+        assertFalse(mailService.sendRecoveryLinkToUser(null));
+    }
+
+    @Test
+    public void doNotSendRecoveryLinkToNullEmailTest() {
+        User user = new User();
+        assertFalse(mailService.sendRecoveryLinkToUser(user));
+    }
+
+    @Test
+    public void doNotChangePasswordIfLinkIsNotValidTest() {
+        ReflectionTestUtils.setField(mailService, "urlRecoveryLink", "link");
+        assertFalse(mailService.changePasswordByToken("anotherLink", anyString()));
+    }
+
+    @Test
+    public void doNotChangePasswordIfTokenIsNotValidTest() {
+        ReflectionTestUtils.setField(mailService, "urlRecoveryLink", "link");
+        ReflectionTestUtils.setField(mailService, "linkExpirationDays", -1);
+        TokenRecovery token = new TokenRecovery();
+        token.setHashEmail("hashEmail");
+        token.setStartTime(new Date());
+        when(tokenService.getTokenByHashEmail("link")).thenReturn(token);
+        assertFalse(mailService.changePasswordByToken("link", anyString()));
+    }
+
+    @Test
+    public void changePasswordIfTokenIsValidTest() {
+        ReflectionTestUtils.setField(mailService, "urlRecoveryLink", "link");
+        ReflectionTestUtils.setField(mailService, "linkExpirationDays", 1);
+        TokenRecovery token = new TokenRecovery();
+        token.setUser(new User());
+        token.setHashEmail("hashEmail");
+        token.setStartTime(new Date());
+        when(tokenService.getTokenByHashEmail("link")).thenReturn(token);
+        assertTrue(mailService.changePasswordByToken("link", anyString()));
+    }
+}
