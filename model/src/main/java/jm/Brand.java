6ef018b5a5b246bd39c9cae540710089ff011bf6
Konstantin <linuksoid@gmail.com>
2020-07-15 02:57:26 +0300

    added entity Brand

---
+++ model/src/main/java/jm/Brand.java
@@ -0,0 +1,23 @@
+package jm;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "brand")
+public class Brand {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+}
