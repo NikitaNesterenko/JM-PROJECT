9291f880fc50f3b3e23c89e029cdf07d15cb0e60
Konstantin Babakov <babakov_kn@navis.ru>
2020-07-20 19:12:41 +0300

    fix package name

--- dao-abstract/src/main/java/jm/stockx/api/dao/ItemDAO.java
+++ dao-abstract/src/main/java/jm/stockx/api/dao/ItemDAO.java
@@ -1,7 +1,7 @@
-package jm.api.dao;
+package jm.stockx.api.dao;
 
-import jm.stockx.entity.Item;
 import jm.stockx.dto.ItemDto;
+import jm.stockx.entity.Item;
 
 import java.util.List;
 import java.util.Optional;
