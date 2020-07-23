70e72cd1abd9a31f830a834eab96223dc6372ace
VasKV <vasily.262@gmail.com>
2020-07-24 00:27:36 +0600

    resolve conflicts improve count in query

--- dao-impl/src/main/java/jm/stockx/api/dao/ItemDaoImpl.java
+++ dao-impl/src/main/java/jm/stockx/api/dao/ItemDaoImpl.java
@@ -33,14 +33,16 @@ public class ItemDaoImpl extends AbstractDAO<Item, Long> implements ItemDAO {
 
     @Override
     public List<Item> getMostPopularItems(String brand) {
-        List<Item> itemList =
-                entityManager.createNativeQuery("SELECT COUNT(i.id) AS i_count" +
-                        " FROM items as i " +
-                        "INNER JOIN buying_item bi on i.id = bi.item_id INNER JOIN brand AS b WHERE b.name =:brand ORDER BY i_count")
-                        .setParameter("brand", brand)
-                        .setMaxResults(10)
-                        .getResultList();
-        return itemList;
+        String sql = "SELECT i.id " +
+                " FROM items as i " +
+                "INNER JOIN buying_item bi on i.id = bi.item_id INNER JOIN brand AS b" +
+                " WHERE b.name =:brand" +
+                " ORDER BY COUNT(i.id) DESC";
+
+        return entityManager.createNativeQuery(sql, Item.class)
+                .setParameter("brand", brand)
+                .setMaxResults(10)
+                .getResultList();
     }
 
     public List<Item> getTopItemsByStyleFromSellingInfo(Long styleId, int topLimit) {
