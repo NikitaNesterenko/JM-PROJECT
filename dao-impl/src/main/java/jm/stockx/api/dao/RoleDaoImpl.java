ead4f02f7f47cb130c1934d5db31dfb43aa59286
bo712 <33088952+bo712@users.noreply.github.com>
2020-07-24 11:22:32 +0300

    remove annotation @Transactional from DaoImpl

--- dao-impl/src/main/java/jm/stockx/api/dao/RoleDaoImpl.java
+++ dao-impl/src/main/java/jm/stockx/api/dao/RoleDaoImpl.java
@@ -2,9 +2,7 @@ package jm.stockx.api.dao;
 
 import jm.stockx.entity.Role;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 @Repository
-@Transactional
 public class RoleDaoImpl extends AbstractDAO<Role, Long> implements RoleDAO {
 }
