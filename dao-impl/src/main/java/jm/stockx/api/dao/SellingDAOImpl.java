3195b32fa5268e9e183d0a45d94ae0f315701dcb
demotivirus <demotivirus@gmail.com>
2020-07-26 23:13:00 +0300

    Создал рест, всё работает как надо, но пока не знаю как вывести количество проданных товаров Вывод типа: [{"userName":"vasya.pupkin","countOfSell":null},{"userName":"petya.vakulov","countOfSell":null},{"userName":"semen.semenych","countOfSell":null},{"userName":"mikailo.lomonosov","countOfSell":null}]

--- dao-impl/src/main/java/jm/stockx/api/dao/SellingDAOImpl.java
+++ dao-impl/src/main/java/jm/stockx/api/dao/SellingDAOImpl.java
@@ -1,8 +1,20 @@
 package jm.stockx.api.dao;
 
 import jm.stockx.entity.SellingInfo;
+import jm.stockx.entity.User;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class SellingDAOImpl extends AbstractDAO<SellingInfo, Long> implements SellingDAO {
+
+    @Override
+    public List<User> getTop20SellingUsers(){
+        String sql = "select i.* from selling_info as si left join users as i on si.user_id=i.id " +
+                " group by si.user_id order by count(si.item_id) desc";
+        return entityManager.createNativeQuery(sql, User.class)
+                .setMaxResults(20)
+                .getResultList();
+    }
 }
