4be477cc64363ef26ede969265f1224e5f525cf9
Konstantin <linuksoid@gmail.com>
2020-07-29 02:30:29 +0300

    added RestControllerAdvice (empty)

---
+++ webapp/src/main/java/jm/stockx/controller/rest/RestControllerAdvice.java
@@ -0,0 +1,8 @@
+package jm.stockx.controller.rest;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class RestControllerAdvice extends ResponseEntityExceptionHandler {
+}
