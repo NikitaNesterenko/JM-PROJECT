d34a41a2e69bfd97c01919310b1bc37f36ddc9d4
Konstantin <linuksoid@gmail.com>
2020-07-29 10:31:53 +0300

    renamed RestControllerAdvice to RestExceptionHandler

--- webapp/src/main/java/jm/stockx/controller/rest/RestControllerAdvice.java
+++
@@ -1,8 +0,0 @@
-package jm.stockx.controller.rest;
-
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-@ControllerAdvice
-public class RestControllerAdvice extends ResponseEntityExceptionHandler {
-}
