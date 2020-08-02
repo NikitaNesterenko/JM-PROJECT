10d679c5c8aaf2917d7c4464bdd46bf83fc5cef5
Konstantin <linuksoid@gmail.com>
2020-08-02 13:27:00 +0300

    added getBidDtoById

--- dao-abstract/src/main/java/jm/stockx/api/dao/BidDAO.java
+++ dao-abstract/src/main/java/jm/stockx/api/dao/BidDAO.java
@@ -1,7 +1,10 @@
 package jm.stockx.api.dao;
 
+import jm.stockx.dto.BidDto;
 import jm.stockx.entity.Bid;
 
 public interface BidDAO extends GenericDao<Bid, Long> {
     boolean doesItExistEntity(Long id);
+
+    BidDto getBidDtoById(Long id);
 }
--- dao-impl/src/main/java/jm/stockx/api/dao/BidDaoImpl.java
+++ dao-impl/src/main/java/jm/stockx/api/dao/BidDaoImpl.java
@@ -1,8 +1,23 @@
 package jm.stockx.api.dao;
 
+import jm.stockx.dto.BidDto;
 import jm.stockx.entity.Bid;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class BidDaoImpl extends AbstractDAO<Bid, Long> implements BidDAO {
+    @Override
+    public BidDto getBidDtoById(Long id) {
+        String query = "" +
+                "SELECT new jm.stockx.dto.BidDto(" +
+                "b.id," +
+                "b.price," +
+                "b.success)" +
+                "FROM Bid AS b " +
+                "WHERE id =: id";
+
+        return entityManager.createQuery(query, BidDto.class)
+                .setParameter("id", id)
+                .getSingleResult();
+    }
 }
