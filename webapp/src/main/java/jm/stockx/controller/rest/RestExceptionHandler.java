e0b89385fcf09d671f80fe53fbb2459d9d627a3a
bo712 <33088952+bo712@users.noreply.github.com>
2020-08-03 01:25:29 +0300

    fix wrong exception

--- webapp/src/main/java/jm/stockx/controller/rest/RestExceptionHandler.java
+++ webapp/src/main/java/jm/stockx/controller/rest/RestExceptionHandler.java
@@ -3,6 +3,7 @@ package jm.stockx.controller.rest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.support.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -11,9 +12,9 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(value = {IllegalArgumentException.class, IllegalStateException.class})
-    protected ResponseEntity<Object> handleConflict(RuntimeException ex, WebRequest request) {
-        String bodyOfResponse = "Wrong argument";
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    protected ResponseEntity<Object> handleException(RuntimeException ex, WebRequest request) {
+        String bodyOfResponse = ex.getMessage();
         return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
     }
 }
