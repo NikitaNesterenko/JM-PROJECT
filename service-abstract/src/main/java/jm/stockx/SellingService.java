29332d4cd4d7f6d758948cfd654c363be1bd2110
aturdaly <aturdaly@gmail.com>
2020-08-06 18:02:07 +0600

    создал рест для обновление статусов для SellingService

--- service-abstract/src/main/java/jm/stockx/SellingService.java
+++ service-abstract/src/main/java/jm/stockx/SellingService.java
@@ -1,5 +1,8 @@
 package jm.stockx;
 
+import jm.stockx.enums.Status;
+
 public interface SellingService {
     Double getAverageSalesValue();
+    void updateSellingStatus(Long id, Status status);
 }
