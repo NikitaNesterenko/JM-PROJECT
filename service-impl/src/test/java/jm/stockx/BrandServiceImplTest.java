026c8eca75867136ffe6e5a9b8cb6925c2b766fa
Anton Melchakov <anton.melchakov1@gmail.com>
2020-08-29 21:16:13 +0500

    Initial commit

---
+++ service-impl/src/test/java/jm/stockx/BrandServiceImplTest.java
@@ -0,0 +1,40 @@
+package jm.stockx;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BrandServiceImplTest {
+
+    @Test
+    public void getAll() {
+    }
+
+    @Test
+    public void get() {
+    }
+
+    @Test
+    public void create() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void getBrandByName() {
+    }
+
+    @Test
+    public void isBrandExist() {
+    }
+
+    @Test
+    public void getBrandDtoById() {
+    }
+}
