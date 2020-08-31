08f954b145cc4ae5e3dbfbfceba9cbf7331e2934
Dimon-16 <dimonchernenko16@gmail.com>
2020-08-31 18:50:19 +0300

    doing

--- dao-impl/src/main/java/jm/stockx/api/dao/UserPortfolioDaoImpl.java
+++ dao-impl/src/main/java/jm/stockx/api/dao/UserPortfolioDaoImpl.java
@@ -12,29 +12,33 @@ import java.util.List;
 public class UserPortfolioDaoImpl extends AbstractDAO<UserPortfolio, Long> {
 
     public UserPortfolio getUserPortfolioByUserId(Long id) {
-        String query = "select user_port FROM UserPortfolio as user_port where user_port.id = :id";
-        return  (UserPortfolio) entityManager.createQuery(query)
+        return  entityManager.createQuery("" +
+                "SELECT user_port " +
+                "FROM   UserPortfolio AS user_port " +
+                "WHERE  user_port.id = :id", UserPortfolio.class)
                 .setParameter("id", id)
                 .getSingleResult();
 
     }
 
     public List<BuyingInfo> getBuyingInfoByPortfolioId(Long id) {
-        String query = "select us.buyingInfo from UserPortfolio as up  inner join up.user as us where up.id = :id";
-        return  entityManager.createQuery(query)
+        return  entityManager.createQuery("" +
+                "SELECT     us.buyingInfo " +
+                "FROM       UserPortfolio AS up " +
+                "INNER JOIN up.user AS us " +
+                "WHERE      up.id = :id")
                 .setParameter("id", id)
                 .getResultList();
 
     }
 
     public List<Item> getItemByPortfolioId(Long id) {
-        String query = "" +
-                "select bi.boughtItems " +
-                "from UserPortfolio as up " +
-                "left join up.user as us " +
-                "join us.buyingInfo as bi " +
-                "where up.id = :id";
-        return entityManager.createQuery(query)
+        return entityManager.createQuery("" +
+                "SELECT    bi.boughtItems " +
+                "FROM      UserPortfolio AS up " +
+                "LEFT JOIN up.user AS us " +
+                "JOIN      us.buyingInfo AS bi " +
+                "WHERE     up.id = :id")
                 .setParameter("id", id)
                 .getResultList();
     }
