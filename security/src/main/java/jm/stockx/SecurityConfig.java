e5f13278f160d166ad049647d6f9ce7f87fee869
Alexei Baranov <baranov.alexalex@gmail.com>
2020-09-18 01:56:55 +0700

    test resolve commit

--- security/src/main/java/jm/stockx/SecurityConfig.java
+++ security/src/main/java/jm/stockx/SecurityConfig.java
@@ -9,9 +9,12 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -39,39 +42,26 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebM
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring().antMatchers(
-                // Vaadin Flow static resources //
                 "/VAADIN/**",
-
-                // the standard favicon URI
+                "/brand/all/**",
                 "/favicon.ico",
-
-                // the robots exclusion standard
                 "/robots.txt",
-
-                // web application manifest //
                 "/manifest.webmanifest",
                 "/sw.js",
                 "/offline-page.html",
-
-                // (development mode) static resources //
                 "/frontend/**",
-
-                // (development mode) webjars //
                 "/webjars/**",
-
-                // (production mode) static resources //
                 "/frontend-es5/**", "/frontend-es6/**");
     }
-
-
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http.formLogin()
-                .loginPage("/login")
-                .loginProcessingUrl("/login")
-                .permitAll();
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/rest/api/**", "/registration/**", "/authorization/**", "/password-recovery/**", "/brand/all", "/news").permitAll()
+                .antMatchers("/user/**").hasAnyAuthority("ADMIN", "USER")
+                .antMatchers("/admin/**").hasAuthority("ADMIN");
 
         http.csrf().disable()
                 .requestCache().requestCache(new CustomRequestCache())
