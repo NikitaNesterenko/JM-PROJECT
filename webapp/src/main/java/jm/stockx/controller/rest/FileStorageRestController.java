080fa6291c40d5512c36d5cbc2887c1970fcb847
Nikita <63064209+WhaleTee@users.noreply.github.com>
2020-09-21 20:50:16 +0300

    Для работы с .png файлами.

--- webapp/src/main/java/jm/stockx/controller/rest/FileStorageRestController.java
+++ webapp/src/main/java/jm/stockx/controller/rest/FileStorageRestController.java
@@ -5,14 +5,19 @@ import jm.stockx.entity.Item;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @RestController
 public class FileStorageRestController {
@@ -32,8 +37,7 @@ public class FileStorageRestController {
 
 
     @PostMapping("/img/upload")
-    public String uploadItemPicture(@RequestParam("id") Long id,
-                                    MultipartFile file) throws IOException {
+    public String uploadItemPicture(@RequestParam("id") Long id, MultipartFile file) throws IOException {
         if (file != null) {
             Item item = itemService.getItemById(id);
             File uploadDir = new File(uploadPath);
@@ -47,5 +51,17 @@ public class FileStorageRestController {
             return Path.of(filePath).toAbsolutePath() + hashGenerator(file.getName());
         }
         return "Uploading failed";
+    }
+
+    @GetMapping("/item/img")
+    public String downloadItemPicture(@RequestParam("id") Long id, HttpServletResponse response) throws IOException {
+        Path file = Paths.get(itemService.getItemById(id).getItemImageUrl());
+        if (Files.exists(file)) {
+            response.setContentType("image/png");
+            Files.copy(file, response.getOutputStream());
+        } else {
+            return "File do not exist.";
+        }
+        return "OK!";
     }
 }
