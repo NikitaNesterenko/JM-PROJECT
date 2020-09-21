b24b0a981b5b44fe7386917187ef03b2eea9a587
Alexei Baranov <baranov.alexalex@gmail.com>
2020-09-21 22:38:18 +0700

    Added latest news template

--- webapp/src/main/java/jm/stockx/controller/user/TestTemplate.java
+++ webapp/src/main/java/jm/stockx/controller/user/TestTemplate.java
@@ -2,6 +2,7 @@ package jm.stockx.controller.user;
 
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
+import jm.stockx.components.templates.LatestNews;
 import jm.stockx.components.templates.ProductTile;
 
 @Route("test-template")
@@ -13,7 +14,11 @@ public class TestTemplate extends VerticalLayout {
             "15 sold",
             "images-for-templates/Louis_Vuitton_Key_Pouch_Monogram_Brown.jpg",
             "");
+
+    private LatestNews latestNews = new LatestNews("#", "How to Follow StockX Bag Condition Guidelines",
+            "StockX - 08/31/2020");
+
     public TestTemplate() {
-        add(productTile);
+        add(productTile, latestNews);
     }
 }
