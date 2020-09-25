86f54d5cdde0b71fc3f3fd5dec0bc030e2834d7e
Nikita <63064209+WhaleTee@users.noreply.github.com>
2020-09-25 22:47:01 +0300

    Revert "Создал разметку страницы how-it-works"
    This reverts commit ba70937194eaa42da8ff5e1159dab46eef520199.

--- security/src/main/java/jm/stockx/SecurityConfig.java
+++ security/src/main/java/jm/stockx/SecurityConfig.java
@@ -63,7 +63,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebM
 
         http
                 .authorizeRequests()
-                    .antMatchers("/", "/rest/api/**", "/registration/**", "/authorization/**", "/password-recovery/**", "/brand/all", "/news", "/how-it-works").permitAll()
+                    .antMatchers("/", "/rest/api/**", "/registration/**", "/authorization/**", "/password-recovery/**", "/brand/all", "/news").permitAll()
                     .antMatchers("/user/**").hasAnyAuthority("ADMIN", "USER")
                     .antMatchers("/admin/**").hasAuthority("ADMIN")
                 .anyRequest().authenticated();
