a710d3bfe94bfeaaf679f2e4b61727192b6a1b5f
Nikita <63064209+WhaleTee@users.noreply.github.com>
2020-09-25 22:44:03 +0300

    Revert "test resolve commit"
    This reverts commit b611742e2aa3f0404d786c2845ac5f3b6ceebda2.

--- security/src/main/java/jm/stockx/ConfigureUIServiceInitListener.java
+++ security/src/main/java/jm/stockx/ConfigureUIServiceInitListener.java
@@ -1,28 +1,28 @@
-//package jm.stockx;
-//
-//import com.vaadin.flow.component.UI;
-//import com.vaadin.flow.router.BeforeEnterEvent;
-//import com.vaadin.flow.server.ServiceInitEvent;
-//import com.vaadin.flow.server.VaadinServiceInitListener;
-//import jm.stockx.ui.views.login.LoginView;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//public class ConfigureUIServiceInitListener implements VaadinServiceInitListener {
-//
-//    @Override
-//    public void serviceInit(ServiceInitEvent event) {
-//        event.getSource().addUIInitListener(uiEvent -> {
-//            final UI ui = uiEvent.getUI();
-//            ui.addBeforeEnterListener(this::beforeEnter);
-//        });
-//    }
-//
-//    private void beforeEnter(BeforeEnterEvent event) {
-//        if (!LoginView.class.equals(event.getNavigationTarget())
-//                && !SecurityUtils.isUserLoggedIn()) {
-//            event.rerouteTo(LoginView.class);
-//        }
-//    }
-//}
-//
+package jm.stockx;
+
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.router.BeforeEnterEvent;
+import com.vaadin.flow.server.ServiceInitEvent;
+import com.vaadin.flow.server.VaadinServiceInitListener;
+import jm.stockx.ui.views.login.LoginView;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ConfigureUIServiceInitListener implements VaadinServiceInitListener {
+
+    @Override
+    public void serviceInit(ServiceInitEvent event) {
+        event.getSource().addUIInitListener(uiEvent -> {
+            final UI ui = uiEvent.getUI();
+            ui.addBeforeEnterListener(this::beforeEnter);
+        });
+    }
+
+    private void beforeEnter(BeforeEnterEvent event) {
+        if (!LoginView.class.equals(event.getNavigationTarget())
+                && !SecurityUtils.isUserLoggedIn()) {
+            event.rerouteTo(LoginView.class);
+        }
+    }
+}
+
