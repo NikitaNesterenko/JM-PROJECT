7e365cc7ec2fd9febcc1937156bc8764425b3f33
Franky <Rewq1999.11.09@gmail.com>
2020-10-01 17:20:52 +0300

    Добавил выборку по ItemName,itemImageUrl,lowestAsk

--- dao-impl/src/main/java/jm/stockx/api/dao/ItemInfoDaoImpl.java
+++ dao-impl/src/main/java/jm/stockx/api/dao/ItemInfoDaoImpl.java
@@ -1,7 +1,10 @@
 package jm.stockx.api.dao;
 
+import jm.stockx.dto.ItemInfoGetDto;
 import jm.stockx.entity.ItemInfo;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 @Repository
@@ -10,10 +13,24 @@ public class ItemInfoDaoImpl extends AbstractDAO<ItemInfo, Long> implements Item
     @Override
     public ItemInfo getByItemId(Long itemId) {
         return entityManager.createQuery(
-                        "FROM ItemInfo AS i " +
+                "FROM ItemInfo AS i " +
                         "WHERE i.item.id = :itemId", ItemInfo.class)
                 .setParameter("itemId", itemId)
                 .getSingleResult();
+    }
+
+    @Override
+    public List<ItemInfoGetDto> getListAndOrderByCash(Integer cash) {
+        return entityManager.createQuery("SELECT NEW " +
+                "jm.stockx.dto.ItemInfoGetDto(" +
+                "if.item.name," +
+                "if.item.itemImageUrl," +
+                "if.lowestAsk" +
+                ")" +
+                "FROM ItemInfo if " +
+                "WHERE if.price > :cash", ItemInfoGetDto.class)
+                .setParameter("cash", cash)
+                .getResultList();
     }
 
 }
