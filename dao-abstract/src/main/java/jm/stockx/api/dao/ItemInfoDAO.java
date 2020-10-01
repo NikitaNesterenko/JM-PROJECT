d7bca1babf23178ab7a238ddd47d207d6abd35ec
Franky <Rewq1999.11.09@gmail.com>
2020-10-01 23:59:00 +0300

    Добавил выборку по ItemName,itemImageUrl,lowestAsk

--- dao-abstract/src/main/java/jm/stockx/api/dao/ItemInfoDAO.java
+++ dao-abstract/src/main/java/jm/stockx/api/dao/ItemInfoDAO.java
@@ -1,13 +1,8 @@
 package jm.stockx.api.dao;
 
-import jm.stockx.dto.ItemInfoGetDto;
 import jm.stockx.entity.ItemInfo;
-
-import java.util.List;
 
 public interface ItemInfoDAO extends GenericDao<ItemInfo, Long>{
 
     ItemInfo getByItemId(Long itemId);
-
-    List<ItemInfoGetDto> getListAndOrderByCash(Integer cash);
 }
