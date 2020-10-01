6b2b19524eb0d689aed57d4964c475d6b1e881f4
Franky <Rewq1999.11.09@gmail.com>
2020-10-02 00:15:48 +0300

    Добавил выборку по ItemName,itemImageUrl,lowestAsk

--- dao-impl/src/main/java/jm/stockx/api/dao/ItemInfoDaoImpl.java
+++ dao-impl/src/main/java/jm/stockx/api/dao/ItemInfoDaoImpl.java
@@ -12,8 +12,8 @@ public class ItemInfoDaoImpl extends AbstractDAO<ItemInfo, Long> implements Item
 
     @Override
     public ItemInfo getByItemId(Long itemId) {
-        return entityManager.createQuery(
-                "FROM ItemInfo AS i " +
+        return entityManager.createQuery("" +
+                        "FROM ItemInfo AS i " +
                         "WHERE i.item.id = :itemId", ItemInfo.class)
                 .setParameter("itemId", itemId)
                 .getSingleResult();
