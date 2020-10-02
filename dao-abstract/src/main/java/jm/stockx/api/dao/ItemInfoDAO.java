0ebd45b4e3cfce5611c76fd477012e9f72daff96
amand <amandoz@list.ru>
2020-10-02 23:18:43 +0300

    Create ItemCategory(enum) and ItemCategoryDto, add enum ItemCategory in Item, add query for ItemInfo by ItemCategory

--- dao-abstract/src/main/java/jm/stockx/api/dao/ItemInfoDAO.java
+++ dao-abstract/src/main/java/jm/stockx/api/dao/ItemInfoDAO.java
@@ -1,9 +1,10 @@
 package jm.stockx.api.dao;
 
+
 import jm.stockx.dto.ItemCategoryDto;
+import jm.stockx.dto.ItemInfoGetDto;
 import jm.stockx.entity.ItemInfo;
 import jm.stockx.enums.ItemCategory;
-
 import java.util.List;
 
 public interface ItemInfoDAO extends GenericDao<ItemInfo, Long> {
@@ -11,4 +12,8 @@ public interface ItemInfoDAO extends GenericDao<ItemInfo, Long> {
     ItemInfo getByItemId(Long itemId);
 
     List<ItemCategoryDto> getItemCategoryDtoByCategory(ItemCategory category);
+
+
+    List<ItemInfoGetDto> getListAndOrderByCash(Integer cash);
+
 }
