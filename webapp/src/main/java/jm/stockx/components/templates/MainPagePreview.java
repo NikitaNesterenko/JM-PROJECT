f424ad66e943821889cf1014d0216757d19e55d9
Yuri <juran228@gmail.com>
2020-10-04 15:42:45 +0100

    Initial commit

---
+++ webapp/src/main/java/jm/stockx/components/templates/MainPagePreview.java
@@ -0,0 +1,69 @@
+package jm.stockx.components.templates;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.html.Anchor;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.icons.VaadinIcons;
+import com.vaadin.ui.Panel;
+import org.vaadin.addons.searchbox.SearchBox;
+
+@CssImport("./styles/main-page-view.css")
+public class MainPagePreview extends VerticalLayout {
+
+    // A container that is 100% wide by default
+    VerticalLayout layout = new VerticalLayout();
+
+    // label will only take the space it needs
+    Label label1 = new Label("Buy & Sell");
+    Label label2 = new Label("Authentic Sneakers");
+
+
+    public MainPagePreview() {
+        Button searchButton = new Button("Find ");
+        TextField textField = new TextField();
+        textField.setPlaceholder("Search");
+        textField.setClearButtonVisible(true);
+        textField.setWidth("20%");
+        textField.setHeightFull();
+
+        SearchBox searchBox = new SearchBox(VaadinIcons.SEARCH, SearchBox.ButtonPosition.RIGHT);
+        searchBox.setButtonJoined(true);
+        searchBox.setPlaceholder("Search...");
+
+        layout.add(label1);
+        layout.add(label2);
+        layout.add(textField);
+
+        layout.add(searchButton);
+
+        add(layout);
+        configureLayout();
+    }
+
+    public void configureLayout(){
+        layout.setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        label1.getStyle().set("font-size", "43pt");
+        label1.getStyle().set("font-color","black");
+
+        label2.getStyle().set("font-size", "43pt");
+        label2.getElement().getStyle().set("color", "white");
+        label2.getStyle().set("background-color","black");
+
+        layout.getStyle().set("background-image", "url(//stockx-assets.imgix.net/Core/sneaker_plain.png?auto=compress,format)");
+        layout.getStyle().set("background-repeat", "no-repeat");
+        layout.getStyle().set("background-size", "100%");
+        layout.getStyle().set("background-position-x", "center");
+        layout.setWidth("100%");
+        layout.setHeight("620px");
+        layout.getStyle().set("padding", "80px 0");
+    }
+
+
+
+}
--- webapp/src/main/java/jm/stockx/controller/user/MainView.java
+++ webapp/src/main/java/jm/stockx/controller/user/MainView.java
@@ -1,13 +1,25 @@
 package jm.stockx.controller.user;
 
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
+import com.vaadin.icons.VaadinIcons;
+import com.vaadin.ui.Panel;
+import jm.stockx.components.templates.MainPagePreview;
+import org.vaadin.addons.searchbox.SearchBox;
 
 @Route
 public class MainView extends VerticalLayout {
+
     public MainView() {
         add(new Button("Click me", e -> Notification.show("Hello, Spring+Vaadin user!")));
+        add(new MainPagePreview());
     }
+
 }
+
