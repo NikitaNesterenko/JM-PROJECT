4276ad6b86bc27ff68bf984776636ff7be8e984f
amand <amandoz@list.ru>
2020-10-22 13:17:24 +0300

    fix all changes by dao and service

--- dao-impl/src/main/java/jm/stockx/api/dao/ItemDaoImpl.java
+++ dao-impl/src/main/java/jm/stockx/api/dao/ItemDaoImpl.java
@@ -51,32 +51,4 @@ public class ItemDaoImpl extends AbstractDAO<Item, Long> implements ItemDAO {
                 .getSingleResult();
     }
 
-
-//    @Override
-//    public List<Item> getMostPopularItemByBrandName(String name) {
-//        return entityManager.createQuery("" +
-//                "SELECT i FROM Item i " +
-//                "INNER JOIN BuyingInfo AS bi " +
-//                "ON i.id = bi.id " +
-//                "INNER JOIN Brand AS b " +
-//                "WHERE b.name =:name " +
-//                "ORDER BY COUNT(i.id) DESC", Item.class)
-//                .setParameter("name", name)
-//                .setMaxResults(10)
-//                .getResultList();
-//    }
-//
-//    @Override
-//    public List<Item> getMostPopularItemByStyleId(Long id, int topLimit) {
-//        return entityManager.createQuery("" +
-//                "SELECT si FROM SellingInfo si " +
-//                "LEFT JOIN Item AS i " +
-//                "ON si.id=i.id " +
-//                "WHERE i.style = :styleId " +
-//                "GROUP BY si.id " +
-//                "ORDER BY count(si.id) DESC", Item.class)
-//                .setParameter("styleId", id)
-//                .setMaxResults(topLimit)
-//                .getResultList();
-//    }
 }
