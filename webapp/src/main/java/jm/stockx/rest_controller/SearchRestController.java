2ec4622270605b84e33550c431871c2b41a075a8
amand <amandoz@list.ru>
2020-10-22 13:20:27 +0300

    fix all changes by dao and service

--- webapp/src/main/java/jm/stockx/rest_controller/SearchRestController.java
+++ webapp/src/main/java/jm/stockx/rest_controller/SearchRestController.java
@@ -5,12 +5,14 @@ import jm.stockx.ItemService;
 import jm.stockx.dto.itemInfo.ItemSearchDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/item")
 public class SearchRestController {
     private final ItemInfoService itemInfoService;
 
