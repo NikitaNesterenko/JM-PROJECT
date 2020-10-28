f5637dd8634a0603cef046c33fbb651c3eeeb012
admin <admin>
2020-10-28 18:01:42 +0300

    Without Main

--- dao-impl/src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-import jm.stockx.api.dao.SellingInfoDAO;
-import jm.stockx.api.dao.SellingInfoDaoImpl;
-
-public class Main {
-    public static void main(String[] args) {
-        SellingInfoDAO sid = new SellingInfoDaoImpl();
-        Double min = sid.getMinSalesValue(1L);
-    }
-}
