b7eb37206d882f75f1872864f77a5dc530ab1d7a
lis154 <lis154@mail.ru>
2020-11-30 19:39:42 +0300

    codeStyle добавил NotificationInfoDTO Изменил метод change, с использованием NotificationInfoDTO

---
+++ model/src/main/java/jm/stockx/dto/notification/NotificationInfoDTO.java
@@ -0,0 +1,16 @@
+package jm.stockx.dto.notification;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class NotificationInfoDTO {
+    String name;
+    Long UserId;
+    boolean state;
+
+
+}
--- webapp/src/main/java/jm/stockx/rest_controller/NotificationInfoRestController.java
+++ webapp/src/main/java/jm/stockx/rest_controller/NotificationInfoRestController.java
@@ -1,20 +1,16 @@
 package jm.stockx.rest_controller;
 
 import jm.stockx.NotificationInfoService;
-import jm.stockx.entity.NotificationInfo;
+import jm.stockx.dto.notification.NotificationInfoDTO;
 import jm.stockx.util.Response;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/notification")
 public class NotificationInfoRestController {
 
     private final NotificationInfoService notificationInfoService;
-    private NotificationInfo notificationInfo;
 
     @Autowired
     public NotificationInfoRestController(NotificationInfoService notificationInfoService) {
@@ -22,10 +18,8 @@ public class NotificationInfoRestController {
     }
 
     @PostMapping("/change")
-    public Response<?> changeNotificationInfo(@RequestParam(name = "nameField") String nameField, @RequestParam(name = "userId") String userId, @RequestParam(name = "state") String state) {
-        Long userIdN = Long.valueOf(userId);
-        boolean stateN = Boolean.parseBoolean(state);
-        notificationInfoService.updateField(userIdN, nameField, stateN);
+    public Response<?> changeNotificationInfo(@RequestBody NotificationInfoDTO notificationInfoDTO) {
+        notificationInfoService.updateField(notificationInfoDTO.getUserId(), notificationInfoDTO.getName(), notificationInfoDTO.isState());
         return Response.ok().build();
     }
 
