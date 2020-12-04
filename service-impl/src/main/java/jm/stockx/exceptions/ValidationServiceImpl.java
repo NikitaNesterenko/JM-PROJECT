e9493a8fbf33dc95ffe0cb1920e511d406edb874
lis154 <lis154@mail.ru>
2020-12-04 18:50:17 +0300

    change DTO

--- service-impl/src/main/java/jm/stockx/exceptions/ValidationServiceImpl.java
+++ service-impl/src/main/java/jm/stockx/exceptions/ValidationServiceImpl.java
@@ -1,7 +1,21 @@
 package jm.stockx.exceptions;
 
+import jm.stockx.AuthorizationAdviceException;
 import jm.stockx.ValidationService;
+import jm.stockx.api.dao.UserDAO;
+import jm.stockx.dto.user.UserDto;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class ValidationServiceImpl implements ValidationService {
-    
+    @Autowired
+    private UserDAO userDAO;
+
+    public boolean validateUserDB(String email) throws AuthorizationAdviceException {
+
+        UserDto userDto = userDAO.getUserDtoByUserEmail(email);
+        if (userDto == null) {
+            throw new AuthorizationAdviceException();
+        }
+        return true;
+    }
 }
