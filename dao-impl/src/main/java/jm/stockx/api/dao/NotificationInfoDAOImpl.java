e38dcabeb128689783d9363b285889ba1932d41c
lis154 <lis154@mail.ru>
2020-12-04 22:26:31 +0300

    final

--- dao-impl/src/main/java/jm/stockx/api/dao/NotificationInfoDAOImpl.java
+++ dao-impl/src/main/java/jm/stockx/api/dao/NotificationInfoDAOImpl.java
@@ -5,17 +5,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class NotificationInfoDAOImpl extends AbstractDAO<NotificationInfo, Long> implements NotificationInfoDAO {
-//    @Override
-//    public void updateField(Long userId, String nameField, boolean state) {
-//                entityManager.createQuery("" +
-//                "UPDATE NotificationInfo notificationInfo SET " +
-//                "notificationInfo." + nameField + " = :state " +
-//                "WHERE notificationInfo.user.id = :userId")
-//                .setParameter("state", state)
-//                .setParameter("userId", userId)
-//                .executeUpdate();
-//    }
-
     @Override
     public void updateField(Long userId, String nameField) {
         entityManager.createNativeQuery("" +
