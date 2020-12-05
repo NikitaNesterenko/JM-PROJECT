fe99614c4e30278ddb7d9727caff9fbace0a2527
lis154 <lis154@mail.ru>
2020-12-05 17:10:25 +0300

    добавление api + spring config for test

--- webapp/src/main/java/jm/stockx/rest_controller/FileStorageRestController.java
+++ webapp/src/main/java/jm/stockx/rest_controller/FileStorageRestController.java
@@ -6,15 +6,13 @@ import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 
 @RestController
+@RequestMapping("/api/item")
 public class FileStorageRestController {
     private FileStorageService fileStorageService;
 
@@ -23,12 +21,12 @@ public class FileStorageRestController {
         this.fileStorageService = fileStorageService;
     }
 
-    @PostMapping("/item/img/upload")
+    @PostMapping("/img/upload")
     public String uploadItemPicture(@RequestParam("id") Long id, MultipartFile file) {
         return fileStorageService.storeFile(id, file);
     }
 
-    @GetMapping("/item/img/download")
+    @GetMapping("/img/download")
     public ResponseEntity<Resource> downloadItemPicture(@RequestParam("filename") String filename,
                                                         HttpServletRequest request)  throws Exception{
         Resource resource = fileStorageService.loadFileAsResource(filename);
