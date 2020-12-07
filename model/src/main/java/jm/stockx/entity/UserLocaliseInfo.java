de84187615eaf4c736ea149856db7dcc6eaa7df8
KirilkoIA <66704958+KirilkoIA@users.noreply.github.com>
2020-12-08 01:55:30 +0300

    Добавление таблицы UserLocaliseInfo и перечислений стран, языков и валют

---
+++ model/src/main/java/jm/stockx/entity/UserLocaliseInfo.java
@@ -0,0 +1,37 @@
+package jm.stockx.entity;
+
+import jm.stockx.enums.UserCountry;
+import jm.stockx.enums.UserCurrency;
+import jm.stockx.enums.UserLanguage;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+@Table(name = "localise_info")
+public class UserLocaliseInfo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_country")
+    @Enumerated(EnumType.STRING)
+    private UserCountry userCountry;
+
+    @Column(name = "user_currency")
+    @Enumerated(EnumType.STRING)
+    private UserCurrency userCurrency;
+
+    @Column(name = "user_language")
+    @Enumerated(EnumType.STRING)
+    private UserLanguage userLanguage;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+}
