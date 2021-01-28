4a8f654675426542487fde0ed306ed72a7848477
esazikov <understander@mail.ru>
2021-01-28 16:34:51 +0500

    VK OAuth2 authorization - содрал всё с гуглОАуф.

--- security/src/main/java/jm/stockx/oAuth2/VkOAuth.java
+++ security/src/main/java/jm/stockx/oAuth2/VkOAuth.java
@@ -1,11 +1,25 @@
 package jm.stockx.oAuth2;
 
+import com.github.scribejava.apis.GoogleApi20;
+import com.github.scribejava.apis.VkontakteApi;
+import com.github.scribejava.core.builder.ServiceBuilder;
+import com.github.scribejava.core.model.OAuth2AccessToken;
+import com.github.scribejava.core.model.OAuthRequest;
+import com.github.scribejava.core.model.Response;
+import com.github.scribejava.core.model.Verb;
 import com.github.scribejava.core.oauth.OAuth20Service;
 import jm.stockx.RoleService;
+import jm.stockx.UserNotFoundException;
 import jm.stockx.UserService;
+import jm.stockx.entity.User;
 import jm.stockx.jwt.JwtTokenProvider;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
 
 @Service
 public class VkOAuth {
@@ -26,10 +40,59 @@ public class VkOAuth {
     @Value("${vk.response_type}")
     private String response_type;
 
+    @Value("${vk.clientSecret}")
+    private String clientSecret;
+
+    User user;
+
     public VkOAuth(UserService userService, RoleService roleService, JwtTokenProvider jwtTokenProvider) {
         this.userService = userService;
         this.roleService = roleService;
         this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @PostConstruct
+    private void init() {
+        auth20Service = new ServiceBuilder(client_id)
+                .apiSecret(clientSecret)
+                .defaultScope(scope)
+                .callback(redirect_uri)
+                .build(VkontakteApi.instance());
+    }
+
+    public String getAuthorizeUrl() {
+        return auth20Service.getAuthorizationUrl();
+    }
+
+    public OAuth2AccessToken getOAuthToken(String code) throws InterruptedException, ExecutionException, IOException {
+        return auth20Service.getAccessToken(code);
+    }
+
+    public String getVkUserToken(OAuth2AccessToken token) throws InterruptedException, ExecutionException, IOException, UserNotFoundException {
+
+        OAuthRequest request = new OAuthRequest(Verb.GET,  "https://oauth.vk.com/authorize");
+        auth20Service.signRequest(token, request);
+        Response response = auth20Service.execute(request);
+
+        JSONObject jsonObj = new JSONObject(response.getBody());
+
+        String firstName = jsonObj.optString("given_name");
+        String lastName = jsonObj.optString("family_name");
+        String email = jsonObj.optString("email");
+
+        if (userService.isUserExistByEmail(email)) {
+            try {
+                user = userService.getUserByEmail(email);
+            } catch (UserNotFoundException e) {
+                e.printStackTrace();
+            }
+        } else {
+            user = new User(firstName, lastName, email, basicPassword);
+            user.setRole(roleService.getRole("ROLE_USER"));
+            userService.createUser(user);
+        }
+
+        return jwtTokenProvider.createToken(email, user.getRole());
     }
 
 
