c44475d42540023f59bd4ada529337f2d07f3881
JMZEN <lamp507@yandex.ru>
2021-02-03 19:48:57 +0300

    [feature] Добавлена логика заполнения Main Page: Brands News Bids Top Selling Items

--- webapp/src/main/java/jm/stockx/rest_controller/ItemRestController.java
+++ webapp/src/main/java/jm/stockx/rest_controller/ItemRestController.java
@@ -2,9 +2,11 @@ package jm.stockx.rest_controller;
 
 import com.google.gson.Gson;
 import jm.stockx.ItemInfoService;
+import jm.stockx.SellingInfoService;
 import jm.stockx.dto.itemInfo.ItemInfoDto;
 import jm.stockx.dto.itemInfo.ItemInfoDtoDecimal;
 import jm.stockx.dto.itemInfo.ItemSearchDto;
+import jm.stockx.dto.sellingInfo.ItemTopInfoDto;
 import jm.stockx.entity.ItemInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -16,11 +18,13 @@ import java.util.List;
 @RequestMapping("/api/item")
 public class ItemRestController {
     private final ItemInfoService itemInfoService;
+    private final SellingInfoService sellingInfoService;
     private static final Gson gson = new Gson();
 
-    @Autowired
-    public ItemRestController(ItemInfoService itemInfoService) {
+    public ItemRestController(ItemInfoService itemInfoService,
+                              SellingInfoService sellingInfoService) {
         this.itemInfoService = itemInfoService;
+        this.sellingInfoService = sellingInfoService;
     }
 
     @GetMapping("/search")
@@ -29,17 +33,20 @@ public class ItemRestController {
     }
 
     @GetMapping("/allItem")
-    public ResponseEntity<List<ItemInfoDtoDecimal>> getAllItemInfo(){
+    public ResponseEntity<List<ItemInfoDtoDecimal>> getAllItemInfo() {
 
         List<ItemInfoDtoDecimal> list = itemInfoService.getAllItemInfoDtoDecimal();
         return ResponseEntity.ok(list);
     }
 
     @GetMapping("/item")
-    public ResponseEntity<ItemInfoDtoDecimal> getItemInfoDtoDec(@RequestParam (name = "id") Long id){
+    public ResponseEntity<ItemInfoDtoDecimal> getItemInfoDtoDec(@RequestParam(name = "id") Long id) {
         return ResponseEntity.ok(itemInfoService.getItemInfoDtoDec(id));
     }
 
-
-
+    @GetMapping("/mostpopular")
+    public ResponseEntity<List<ItemTopInfoDto>> getMostPopularItems() {
+        List<ItemTopInfoDto> foundTodItems = sellingInfoService.getItemTopInfoDto(5);
+        return ResponseEntity.ok(foundTodItems);
+    }
 }
