9cedc283735d1f4d0d763ad90f14588466785c09
Kirill Kuzmenko <kirill174ru@gmail.com>
2021-02-10 11:53:44 +0300

    Переделал связи ItemInfo с другими сущностями

--- model/src/main/java/jm/stockx/entity/Item.java
+++ model/src/main/java/jm/stockx/entity/Item.java
@@ -9,7 +9,18 @@ import lombok.ToString;
 import org.hibernate.annotations.TypeDef;
 import org.jadira.usertype.moneyandcurrency.joda.PersistentMoneyAmountAndCurrency;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
+import javax.persistence.OneToOne;
+import java.io.Serializable;
+
 
 @Getter
 @Setter
@@ -21,7 +32,7 @@ import javax.persistence.*;
 @Table(name = "items")
 @TypeDef(name = "joda_MoneyAmountWithCurrencyType", typeClass = PersistentMoneyAmountAndCurrency.class)
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-public class Item {
+public class Item implements Serializable {
 
     @Id
     @Column(name = "id")
